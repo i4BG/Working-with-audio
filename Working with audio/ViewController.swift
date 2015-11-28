94f8bd91907066ff6e0b028d284ace9d4dcac18e
John Appleseed <activate.phone@icloud.com>
2015-11-28 16:38:42 +0200

    Initial Commit

---
+++ Working with audio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Working with audio
+//
+//  Created by Ivaylo Todorov on 28.11.15 г..
+//  Copyright © 2015 г. Ivaylo Todorov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
