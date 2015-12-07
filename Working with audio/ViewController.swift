a39476791fe92c0e9e4c5f07b7e6c340f3df7a8b
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 22:15:23 +0200

    WorkingWithAudio-iOS
    simple playing with audio in iOS

--- Working with audio/ViewController.swift
+++ Working with audio/ViewController.swift
@@ -7,12 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var slider: UISlider!
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        player.play()
+
+        }
+        
+    
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        player.pause()
+        
+    }
+    
+    @IBAction func volume(sender: AnyObject) {
+    
+        player.volume = slider.value
+        
+    }
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let audioPath = NSBundle.mainBundle().pathForResource("dalepapi", ofType: "mp3")!
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+            
+            player.play()
+            
+        } catch {
+            
+            // process eror here
+        
+    }
     }
 
     override func didReceiveMemoryWarning() {
